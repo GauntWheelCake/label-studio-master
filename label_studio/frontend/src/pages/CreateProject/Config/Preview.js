69c25db4953528dea08f7c4db9d92a1984fc9ecd
Lin Qinming <145789568+GauntWheelCake@users.noreply.github.com>
2025-09-25 01:04:45 +0800

    Improve zh-CN i18n initialization and tests

--- label_studio/frontend/src/pages/CreateProject/Config/Preview.js
+++ label_studio/frontend/src/pages/CreateProject/Config/Preview.js
@@ -4,6 +4,7 @@ import { useLibrary } from '../../../providers/LibraryProvider';
 import { cn } from '../../../utils/bem';
 import './Config.styl';
 import { EMPTY_CONFIG } from './Template';
+import { t } from '../../../i18n';
 
 const configClass = cn("configure");
 
@@ -47,7 +48,7 @@ export const Preview = ({ config, data, error }) => {
 
   return (
     <div className={configClass.elem("preview")}>
-      <h3>UI Preview</h3>
+      <h3>{t('createProject.preview.title')}</h3>
       {error && <div className={configClass.elem("preview-error")}>
         <h2>{error.detail} {error.id}</h2>
         {error.validation_errors?.non_field_errors?.map?.(err => <p key={err}>{err}</p>)}
